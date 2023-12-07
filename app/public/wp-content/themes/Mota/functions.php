6e6d41639fe9cdab041ac28223fb080a1e022ebb
florence <f.lanquetin@orange.fr>
2023-12-07 17:45:59 +0100

    07/12/23

---
+++ app/public/wp-content/themes/Mota/functions.php
@@ -0,0 +1,18 @@
+<?php
+// Chargement du style
+function theme_enqueue_styles() {
+wp_enqueue_style('theme', get_template_directory_uri() . '/css/theme.css');
+wp_enqueue_style('style', get_template_directory_uri() . '/style.css');
+// Chargement de Font Awesome pour les icônes
+wp_enqueue_style('font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css', array(), null);
+}
+add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
+
+// Ajout de la gestion des menus dans le tableau de bord de wordpress 
+function register_custom_menus() {
+    register_nav_menus(array(
+        'menu_principal' => __('Menu principal', 'Mota'), 
+        'menu_secondaire' => __('Menu secondaire', 'Mota'),
+    ));
+}
+add_action('init', 'register_custom_menus');
---
+++ app/public/wp-content/themes/Mota/header.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html <?php language_attributes(); ?>>
+
+<!-- Elements repris du theme hello elementor --> 
+    <head>
+        <meta charset="<?php bloginfo( 'charset' ); ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1" /> 
+        <title>Mota</title>
+        <?php wp_head(); ?>
+    </head>
+
+    <body> 
+        <header>
+            <!-- Ajout d'un custom logo modifiable via le tableau de bord --> 
+            <div class="logo-site">
+                <?php the_custom_logo() ?> 
+            </div>
+            <!-- Appel du menu principal modifiable dans le tableau de bord -->
+            <nav>
+                <?php 
+                wp_nav_menu(array(
+                'theme_location' => 'menu_principal', 
+                'container' => false,
+                'menu_class' => 'menu',
+                ));
+                ?> 
+            </nav>
+            <!-- D'autres éléments seront a ajoutés ensuite --> 
+        </header>
+
+        <main>
+        </main>
+
+        <footer>
+            <!-- Menu secondaire -->
+            <div>
+                <?php 
+                    wp_nav_menu(array(
+                        'theme_location' => 'menu_secondaire', 
+                        'container' => false,
+                        'menu_class' => 'menu',
+                    )); 
+                ?>
+            </div>
+        </footer>
+    </body>
+</html>
